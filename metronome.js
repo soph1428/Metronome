1c96a68bc4a95db46be1aef1e0091a3dc4bfc115
soph1428 <sophiagroth16@gmail.com>
2022-10-04 21:15:24 -0500

    update

--- metronome.js
+++ metronome.js
@@ -1,7 +1,7 @@
 var metronomeLabel = document.getElementById(`metronome`),
 clickSound = document.createElement(`audio`),
-metronomeInterval = ``,change = 2,
-started = false, clickSound.src = `click.wav`
+metronomeInterval = ``,change = 2, started = false
+clickSound.src = `click.wav`
 document.getElementById(`start`).onclick = metronome
 document.getElementById(`stop`).onclick = function() {clearInterval(metronomeInterval), started = false, metronomeLabel.style.color = ``}
 function metronome() {
