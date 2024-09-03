759519160c565829eca181f150b0ab6f1bff486d
soph1428 <sophiagroth16@gmail.com>
2024-09-03 01:41:33 -0500

    update

--- metronome.js
+++ metronome.js
@@ -1,6 +1,6 @@
 var metronomeLabel = document.getElementById(`metronome`),
 clickSound = document.createElement(`audio`),
-metronomeInterval = ``, change = 2, started = false
+metronomeInterval = ``, change = 1, started = false
 clickSound.src = `click.mp3`
 document.getElementById(`start`).onclick = metronome
 document.getElementById(`stop`).onclick = function() {clearInterval(metronomeInterval), started = false, metronomeLabel.style.color = `white`}
@@ -14,7 +14,7 @@ function metronome() {
 } document.addEventListener(`keydown`, e => {if (e.key == `ArrowUp`) changeTempo(change)})
 document.addEventListener(`keydown`, e => {if (e.key == `ArrowDown`) changeTempo(-change)})
 function changeTempo(direction) {
-    if (metronomeLabel.textContent.split(` `)[0] - change >= change && (direction == -change || direction == change)) {
+    if (metronomeLabel.textContent.split(` `)[0] - change >= change || direction == change) {
         metronomeLabel.textContent = `${Number(metronomeLabel.textContent.split(` `)[0]) + direction} bpm`
         if (started) metronome()
     }
